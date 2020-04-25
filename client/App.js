c986b23840ce53fc5dc4400049ebd7d5f8b131a0
nishkamk <50085460+nishkamk@users.noreply.github.com>
2020-04-25 13:10:45 -0700

    hi
    Css file (very simple, will be changed)
    JS file updates

--- client/App.js
+++ client/App.js
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  const[symptoms, continualSymptoms] = React.useState([
-    {text: "Symptom: Dizziness"},
-    {text: "Symptom: Nausea"},
-    {text: "Symptom: Headache"},
-  ])   
-  const Symptom = ({symptom}) => <div className ="symptom">{symptom.text}</div>
+
+const Symptom = ({symptom}) => <div className ="symptom">{symptom.text}</div>
+
+function SymptomForm({ addSymptom }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addSymptom(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
 }
 
-return (
-  <div className = "app">
-    <div className = "symptoms-list">
-      {symptoms.map((symptom, index) => (
-        <Symptom
-          key = {index}
-          index = {index}
-          symptom = {symptom}
-        />
-      ))}
+
+function App() {
+  const[symptoms, continualSymptoms] = useState([
+    {text: "Symptom: Bloating"},
+    {text: "Symptom: Cough"},
+    {text: "Symptom: Diarrhea"},
+    {text: "Symptom: Dizziness"},
+    {text: "Symptom: Fatigue"},
+    {text: "Symptom: Fever"},
+    {text: "Symptom: Headache"},
+    {text: "Symptom: Muscle Cramp"},
+    {text: "Symptom: Nausea"},
+    {text: "Symptom: Throat Irritation"},
+    {text: "Symptom: Other"},
+  ])   
+
+  const addSymptom = text => {
+    const newSymptoms = [...symptoms, { text }];
+    continualSymptoms(newSymptoms);
+  };
+
+  return (
+    <div className = "app">
+      <div className = "symptoms-list">
+        {symptoms.map((symptom, index) => (
+          <Symptom
+            key = {index}
+            index = {index}
+            symptom = {symptom}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+  
+}
 
 export default App;
