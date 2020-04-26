ab9f53c14f949d892c392a9424a28f48f13611fc
Bill9000 <williamfeng1729@gmail.com>
2020-04-26 07:24:54 -0700

    Update rapidAPI token with Lucas's changes
    Co-Authored-By: seattle2020 <seattle2020@users.noreply.github.com>

--- server/build/agent/infermAPI.js
+++ server/build/agent/infermAPI.js
@@ -40,55 +40,49 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = __importDefault(require("axios"));
-var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImhvYW5nX3Bob25nOThAbWUuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI2ODg3IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIwLTA0LTI1IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE1ODc5MTc1ODcsIm5iZiI6MTU4NzkxMDM4N30.vk3n2tLc4YNEdB_byVAXKPFC8hTxtqPzTrOFmmB6lZQ';
-var rapidAPI = axios_1.default.create({
-    baseURL: 'https://sandbox-healthservice.priaid.ch/',
+var Key = 'b8c1e15a354c62986e618dcfcbd0c72b';
+var Id = 'e9475a35';
+var infermAPI = axios_1.default.create({
+    baseURL: 'https://api.infermedica.com/covid19/',
     headers: {
         'content-type': 'application/json',
     },
 });
 exports.default = {
-    getSymptoms: function () {
+    getDiagnosisFromInfermedica: function (symptoms, gender, age) {
         return __awaiter(this, void 0, void 0, function () {
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4 /*yield*/, rapidAPI.get('/symptoms', {
-                            params: { token: token, format: 'json', language: 'en-gb' },
+                    case 0: return [4 /*yield*/, infermAPI.post('/diagnosis', {
+                            data: {
+                                'App-Key': Key,
+                                'App-Id': Id,
+                                evidence: symptoms,
+                                sex: gender,
+                                age: age
+                            }
                         })];
                     case 1: return [2 /*return*/, _a.sent()];
                 }
             });
         });
     },
-    getDiagnoses: function (symptoms, gender, birthYear) {
+    getTriageFromInfermedica: function (symptoms, gender, age) {
         return __awaiter(this, void 0, void 0, function () {
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4 /*yield*/, rapidAPI.get('/diagnosis', {
-                            params: {
-                                token: token,
-                                format: 'json',
-                                language: 'en-gb',
-                                symptoms: symptoms,
-                                gender: gender,
-                                year_of_birth: birthYear,
-                            },
+                    case 0: return [4 /*yield*/, infermAPI.post('/triage', {
+                            data: {
+                                'App-Key': Key,
+                                'App-Id': Id,
+                                evidence: symptoms,
+                                sex: gender,
+                                age: age
+                            }
                         })];
                     case 1: return [2 /*return*/, _a.sent()];
                 }
             });
         });
-    },
-    getIssueInfo: function (id) {
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0: return [4 /*yield*/, rapidAPI.get("/issues/" + id + "/info", {
-                            params: { token: token, issue_int: id, language: 'en-gb', format: 'json' },
-                        })];
-                    case 1: return [2 /*return*/, _a.sent()];
-                }
-            });
-        });
-    },
+    }
 };
